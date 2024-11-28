f5b0f54c92cf0c3e8e333e92ec9912f58ead528e
Ehsan Azizyan <ehsanazizyan74@gmail.com>
2024-11-28 18:39:04 +0330

    feat(auth): add register and login API functions
    - Implemented `registerUser` function to handle user registration via API.
    - Implemented `loginUser` function to handle user login and return JWT token.
    - Both functions manage API errors and provide appropriate responses.

---
+++ services/authService.ts
@@ -0,0 +1,61 @@
+import axios, { AxiosError } from "axios";
+
+// Register
+interface RegisterResponse {
+    status: number;
+    message: string;
+}
+
+export async function registerUser(username: string, password: string): Promise<RegisterResponse> {
+    try {
+        const response = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/register`, {
+            username,
+            password,
+        });
+
+        return {
+            status: response?.status || 201,
+            message:
+                `${response?.data?.message} Go to home page` ||
+                "User registered successfully go to home page",
+        };
+    } catch (error: any) {
+        console.log(error);
+        return {
+            status: error?.status || 400,
+            message: error?.response.data?.message || "User already exists",
+        };
+    }
+}
+
+//Login
+interface LoginResponse {
+    status: number;
+    token?: string;
+    message?: string;
+}
+
+interface LoginErrorResponse {
+    message: string;
+}
+
+export const loginUser = async (username: string, password: string): Promise<LoginResponse> => {
+    try {
+        const response = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/login`, {
+            username,
+            password,
+        });
+
+        return {
+            status: response.status,
+            token: response.data?.token,
+        };
+    } catch (error: any) {
+        const axiosError = error as AxiosError<LoginErrorResponse>;
+
+        return {
+            status: axiosError.response?.status || 400,
+            message: axiosError.response?.data?.message || "Invalid username or password",
+        };
+    }
+};
